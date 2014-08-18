029c6ce4fbd147ff90f79891f72094580b2e0d45
moizk <moizkmalik@Lightbulb.local>
2014-08-17 21:57:10 -0400

    horray arrays

--- arrays/MyFirstCProgram/MyFirstCProgram/main.c
+++ arrays/MyFirstCProgram/MyFirstCProgram/main.c
@@ -11,8 +11,16 @@
 int main()
 {
 
-    // insert code here...
-    printf("Hello, World!\n");
+    float numbers_geek_love[] = {3.1415, 1.6180, 1.4142};
+    
+    printf("PI %f\n", numbers_geek_love[0]);
+    printf("Golden ratio %f\n", numbers_geek_love[1]);
+    printf("Square root of 2 %f\n", numbers_geek_love[2]);
+    
+    
+    int primes[] = {2, 3, 5, 7};
+    printf("the first 4 prime numbers %d %d %d %d\n", primes[0], primes[1], primes[2], primes[3]);
+    
     return 0;
 }
 
