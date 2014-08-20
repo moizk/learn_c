798034b704e3145da4505711f411f6196660fcba
Moiz K. Malik <moiz@heuretix.com>
2014-08-19 22:11:59 -0400

    do while loop

--- loops/MyFirstCProgram/MyFirstCProgram/main.c
+++ loops/MyFirstCProgram/MyFirstCProgram/main.c
@@ -11,14 +11,15 @@
 int main()
 {
 
-    int many[] = {2, 4, 8};
-    int sum = 0;
+    char letters[] = {'a', 'b', 'c'};
+    int w=0;
+    
+    do {
+        printf("letter %d is %c\n", w, letters[w]);
+        ++w;
+    } while (w < 3);
     
     
-    for (int i=0; i < 3; i++) {
-        sum +=many[i];
-        printf("i %d sum %d\n", i, sum);
-    }
     return 0;
 }
 
