7ad612efcf71f189645b7ed497850a0578eafdba
Moiz K. Malik <moiz@heuretix.com>
2014-08-19 22:36:59 -0400

    functions

--- functions/MyFirstCProgram/MyFirstCProgram/main.c
+++ functions/MyFirstCProgram/MyFirstCProgram/main.c
@@ -8,11 +8,19 @@
 
 #include <stdio.h>
 
+int funky_math(int a, int b);
+
 int main()
 {
-
-    // insert code here...
-    printf("Hello, World!\n");
+    int foo = 24;
+    int bar = 35;
+    int result = funky_math(foo, bar);
+    int lots = funky_math(foo, bar) * funky_math(foo, bar);
+    printf("funky math %d\n", lots);
     return 0;
+}
+
+int funky_math(int a, int b) {
+    return a+b+343;
 }
 
